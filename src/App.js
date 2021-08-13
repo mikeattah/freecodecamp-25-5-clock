b40375837d4c552c6bb806a264254f6cdfdaf866
Mike Attah <mailenemona@gmail.com>
2021-08-13 10:38:13 +0100

    setup ui layout

--- src/App.js
+++ src/App.js
@@ -13,31 +13,59 @@ class App extends React.Component {
           <h1>25 + 5 Clock</h1>
           <div id="top-controls">
             <div id="break">
-              <p>Break Length</p>
+              <p id="break-label">Break Length</p>
               <div>
-                <i class="fa fa-arrow-down" aria-hidden="true"></i>
-                <span>25</span>
-                <i class="fa fa-arrow-up" aria-hidden="true"></i>
+                <i
+                  id="break-decrement"
+                  class="fa fa-arrow-down"
+                  aria-hidden="true"
+                ></i>
+                <span id="break-length">5</span>
+                <i
+                  id="break-increment"
+                  class="fa fa-arrow-up"
+                  aria-hidden="true"
+                ></i>
               </div>
             </div>
             <div id="session">
-              <p>Session Length</p>
+              <p id="session-label">Session Length</p>
               <div>
-                <i class="fa fa-arrow-down" aria-hidden="true"></i>
-                <span>5</span>
-                <i class="fa fa-arrow-up" aria-hidden="true"></i>
+                <i
+                  id="session-decrement"
+                  class="fa fa-arrow-down"
+                  aria-hidden="true"
+                ></i>
+                <span id="session-length">25</span>
+                <i
+                  id="session-increment"
+                  class="fa fa-arrow-up"
+                  aria-hidden="true"
+                ></i>
               </div>
             </div>
             <div id="display">
-              <span>Session</span>
-              <span>25:00</span>
+              <span id="timer-label">Session</span>
+              <span id="time-left">25:00</span>
             </div>
             <div id="bottom-controls">
-              <div>
+              <div id="start_stop">
                 <i class="fa fa-play" aria-hidden="true"></i>
                 <i class="fa fa-pause" aria-hidden="true"></i>
               </div>
-              <i class="fa fa-refresh" aria-hidden="true"></i>
+              <i id="reset" class="fa fa-refresh" aria-hidden="true"></i>
+            </div>
+            <div id="credit">
+              <p>
+                Designed by{" "}
+                <a href="https://www.freecodecamp.org/no-stack-dub-sack">
+                  Peter Weinberg
+                </a>
+              </p>
+              <p>
+                Coded by{" "}
+                <a href="https://www.freecodecamp.org/mikeattah">Mike Attah</a>
+              </p>
             </div>
           </div>
         </div>

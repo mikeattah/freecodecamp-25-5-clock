0232411e6af0a8bc83d1e1d33bfe2cc9cc064f6e
Mike Attah <mailenemona@gmail.com>
2021-08-14 23:07:53 +0100

    completed clock ui

--- src/App.js
+++ src/App.js
@@ -11,66 +11,78 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>25 + 5 Clock</h1>
-          <div id="clock">
-            <div id="top-controls">
-              <div id="break">
-                <span id="break-label">Break Length</span>
-                <div className="break-display">
-                  <i
-                    id="break-decrement"
-                    className="fa fa-arrow-down"
-                    ariaHidden="true"
-                  ></i>
-                  <span id="break-length">5:00</span>
-                  <i
-                    id="break-increment"
-                    className="fa fa-arrow-up"
-                    ariaHidden="true"
-                  ></i>
+          <div id="box">
+            <h1>25 + 5 Clock</h1>
+            <div id="clock">
+              <div id="top-controls">
+                <div id="break">
+                  <span id="break-label">Break Length</span>
+                  <div className="break-display">
+                    <i
+                      id="break-decrement"
+                      className="fa fa-arrow-down"
+                      ariaHidden="true"
+                    ></i>
+                    <span id="break-length">5:00</span>
+                    <i
+                      id="break-increment"
+                      className="fa fa-arrow-up"
+                      ariaHidden="true"
+                    ></i>
+                  </div>
+                </div>
+                <div id="session">
+                  <span id="session-label">Session Length</span>
+                  <div className="session-display">
+                    <i
+                      id="session-decrement"
+                      className="fa fa-arrow-down"
+                      ariaHidden="true"
+                    ></i>
+                    <span id="session-length">25:00</span>
+                    <i
+                      id="session-increment"
+                      className="fa fa-arrow-up"
+                      ariaHidden="true"
+                    ></i>
+                  </div>
                 </div>
               </div>
-              <div id="session">
-                <span id="session-label">Session Length</span>
-                <div className="session-display">
-                  <i
-                    id="session-decrement"
-                    className="fa fa-arrow-down"
-                    ariaHidden="true"
-                  ></i>
-                  <span id="session-length">25:00</span>
-                  <i
-                    id="session-increment"
-                    className="fa fa-arrow-up"
-                    ariaHidden="true"
-                  ></i>
+              <div id="display">
+                <span id="timer-label">Session</span>
+                <span id="time-left">25:00</span>
+              </div>
+              <div id="bottom-controls">
+                <div id="start_stop">
+                  <i className="fa fa-play" ariaHidden="true"></i>
+                  <i className="fa fa-pause" ariaHidden="true"></i>
+                </div>
+                <div id="reset">
+                  <i className="fa fa-refresh" ariaHidden="true"></i>
                 </div>
               </div>
-            </div>
-            <div id="display">
-              <span id="timer-label">Session</span>
-              <span id="time-left">25:00</span>
-            </div>
-            <div id="bottom-controls">
-              <div id="start_stop">
-                <i className="fa fa-play" ariaHidden="true"></i>
-                <i className="fa fa-pause" ariaHidden="true"></i>
+              <div id="credit">
+                <p>
+                  Designed by{" "}
+                  <a
+                    href="https://www.freecodecamp.org/no-stack-dub-sack"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Peter Weinberg
+                  </a>
+                </p>
+                <p>
+                  Coded by{" "}
+                  <a
+                    href="https://www.freecodecamp.org/mikeattah"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Mike Attah
+                  </a>
+                </p>
               </div>
-              <div id="reset">
-                <i className="fa fa-refresh" ariaHidden="true"></i>
-              </div>
-            </div>
-            <div id="credit">
-              <p>
-                Designed by{" "}
-                <a href="https://www.freecodecamp.org/no-stack-dub-sack">
-                  Peter Weinberg
-                </a>
-              </p>
-              <p>
-                Coded by{" "}
-                <a href="https://www.freecodecamp.org/mikeattah">Mike Attah</a>
-              </p>
             </div>
           </div>
         </div>
